2625930eab07e51128e99bd7a1eebadf52b02d31
JadeRabbitRomy <846261434@qq.com>
2023-08-06 19:32:44 +0800

    first commit init

---
+++ src/components/04-ref/04-Child.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>ref Child:{{ value }}</h1>
+    <input ref="input" type="text" v-model="value" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    focus() {
+      this.$refs.input.focus();
+    },
+  },
+};
+</script>
+
+<style></style>
